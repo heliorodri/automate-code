f31b12f6d822396389406c34c6ff1402706d8667
helio.rodrigues <5Aa7^NG%Pg5jv*p>
2023-03-01 11:55:23 -0300

    add logic to read a question from user input and ask it to AI

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module automate-unit-test
 
 go 1.19
+
+require github.com/PullRequestInc/go-gpt3 v1.1.11
+
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/kkdai/chatgpt v0.2.1 // indirect
+	github.com/spf13/cobra v1.6.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
--- main.go
+++ main.go
@@ -1,14 +1,43 @@
 package main
 
-import "os"
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+
+	gpt3 "github.com/PullRequestInc/go-gpt3"
+)
 
 func main() {
 	apiKey := getApiKey()
+	context := context.Background()
+	client := gpt3.NewClient(apiKey)
 
-	if apiKey != "" {
-		println("done")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("Please, ask your question?")
+	scanner.Scan()
+	question := scanner.Text()
+
+	if question != "" {
+		askAI(client, context, question)
 	}
+}
 
+func askAI(client gpt3.Client, ctx context.Context, question string) {
+	err := client.CompletionStreamWithEngine(ctx, gpt3.TextDavinci003Engine, gpt3.CompletionRequest{
+		Prompt:    []string{question},
+		MaxTokens: gpt3.IntPtr(3000),
+		// Temperature:      new(float32),
+	}, func(resp *gpt3.CompletionResponse) {
+		fmt.Print(resp.Choices[0].Text)
+	})
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(13)
+	}
 }
 
 func getApiKey() string {
